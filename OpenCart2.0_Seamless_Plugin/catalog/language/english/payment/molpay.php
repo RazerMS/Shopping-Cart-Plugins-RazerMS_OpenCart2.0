c934a3c159d6ba845777496fcfc39f39145f7fd0
Mohd Hafeez <mohd.hafeez.johari@gmail.com>
2015-10-26 11:51:30 +0800

    modified language file on frontend site.
    adding all channel to language file.

--- OpenCart2.0_Seamless_Plugin/catalog/language/english/payment/molpay.php
+++ OpenCart2.0_Seamless_Plugin/catalog/language/english/payment/molpay.php
@@ -9,3 +9,26 @@
  
 // Text
 $_['text_title'] = 'MOLPay Malaysia Online Payment Gateway (Visa, MasterCard, Maybank2u, MEPS, FPX, etc )';
+
+// Channel
+$_['channel_list']					= 	[
+											'affinonline' 	=> 'Affin Bank(Affin Online)',
+											'amb' 			=> 'Am Bank (Am Online)',
+											'bankislam' 	=> 'Bank Islam',
+											'cimbclicks' 	=> 'CIMB Bank(CIMB Clicks)',
+											'fpx' 			=> 'MyClear FPX',
+											'hlb' 			=> 'Hong Leong Bank(HLB Connect)',
+											'maybank2u' 	=> 'Maybank(Maybank2u)',
+											'pbb' 			=> 'PublicBank (PBB Online)',
+											'rhb' 			=> 'RHB Bank(RHB Now)',
+											'molwallet' 	=> 'MOLWallet',
+											'cash-711' 		=> '7-Eleven(MOLPay Cash)',
+											'credit' 		=> 'Credit Card/ Debit Card',
+											'ATMVA' 		=> 'ATM Transfer via Permata Bank',
+											'dragonpay' 	=> 'Dragonpay',
+											'paysbuy' 		=> 'PaysBuy',
+											'Point-BCard' 	=> 'Bcard points',
+											'credit3' 		=> 'Multi Currency Credit Card',
+											'NGANLUONG' 	=> 'NGANLUONG',
+											'crossborder' 	=> 'Credit Card/ Debit Card (PayVision)'
+										];
