6e41ec65ff4d0e63eaf03ca40e63db9b03fe4564
amaleena123 <lina.webdev@gmail.com>
2017-09-25 11:37:19 +0800

    Add channel alipay
    Add channel alipay

--- OpenCart2.0_Seamless_Plugin/admin/language/english/payment/molpay.php
+++ OpenCart2.0_Seamless_Plugin/admin/language/english/payment/molpay.php
@@ -54,7 +54,8 @@ $_['channel_list']					= 	array(
 											'crossborder' 	=> 'Credit Card/ Debit Card (PayVision)',
 											'PEXPLUS' 	=> 'PEx+',
 											'jompay' 	=> 'JOMPay',
-											'Cash-Esapay' 	=> 'Cash Esapay'
+											'Cash-Esapay' 	=> 'Cash Esapay',
+											'alipay'	=> 'Alipay.com'
 										);
 
 // Help
